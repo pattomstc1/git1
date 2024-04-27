7059e5cd922fe63f2c607743e31a60105c4c7e4b
sree <v.s.sreeram1996@gmail.com>
2024-04-27 11:57:25 +0530

    edited

--- git1/src/git1/Git1.java
+++ git1/src/git1/Git1.java
@@ -4,7 +4,8 @@ public class Git1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+System.out.println("hello");
+System.out.println("stc");
 	}
 
 }
